bfa1b4280157fe2b397f6086b05d558d308edf1d
Matteo Bandiera <misterflag86@gmail.com>
2015-05-02 21:41:27 +0100

    Removed comments

--- js/init.js
+++ js/init.js
@@ -142,16 +142,6 @@
                         });
                         
 	});
-//        $(function(){
-//            $(".progress > span").each( function() {
-//            $(this)
-//               .data("origWidth", $(this).width())
-//               .width(0)
-//               .animate({
-//                 width: $(this).data("origWidth") + "%" // or + "%" if fluid
-//               }, 1200);
-//         });
-//        });
         
         $(document).ready(function() {
 	
